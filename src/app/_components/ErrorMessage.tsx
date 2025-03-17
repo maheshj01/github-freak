c464bf3763c6ae3ad504c83962a9965e7b8811d0
mahesh jamdade <maheshmn121@gmail.com>
2025-03-17 00:11:06 -0400

    minor ui tweak

--- src/app/_components/ErrorMessage.tsx
+++ src/app/_components/ErrorMessage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { FaExclamationCircle } from "react-icons/fa";
+import { useTheme } from '../context/AppThemeProvider';
 
 interface ErrorMessageProps {
     username: string;
@@ -8,18 +9,19 @@ interface ErrorMessageProps {
 }
 
 const ErrorMessage: React.FC<ErrorMessageProps> = ({ username, message }) => {
+    const { theme } = useTheme();
+    const isDark = theme.mode === 'dark';
     return (
         <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            className="flex flex-col items-center justify-center p-8 rounded-lg bg-red-50 dark:bg-red-900/20"
-        >
-            <FaExclamationCircle className="text-6xl text-red-500 dark:text-red-400 mb-4" />
-            <h2 className="text-2xl font-bold text-red-700 dark:text-red-300 mb-2">User Not Found</h2>
-            <p className="text-red-600 dark:text-red-200 text-center">
-                {message || `We couldn't find a GitHub user with the username "${username}". Please check the spelling and try again.`}
+            className="flex flex-col items-center justify-center p-8 ">
+            <h2 className={`text-2xl font-bold ${isDark ? 'text-white' : 'text-red-600 '} mb-2`}>User Not Found</h2>
+            <p className={`text-center ${isDark ? 'text-white' : 'text-red-600 '}`} >
+                {message || `We couldn't find a GitHub user with the username "${username}".`}
             </p>
-        </motion.div>
+            <p className={`text-center ${isDark ? 'text-white' : 'text-red-600 '}`}> Please check the spelling and try again.</p>
+        </motion.div >
     );
 };
 
