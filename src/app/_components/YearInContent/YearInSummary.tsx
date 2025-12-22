543e96d63e5e183e33ce4dbb06775e9b49d07ce9
mahesh jamdade <maheshmn121@gmail.com>
2025-12-22 01:59:01 -0500

    add new line

--- src/app/_components/YearInContent/YearInSummary.tsx
+++ src/app/_components/YearInContent/YearInSummary.tsx
@@ -66,8 +66,8 @@ const YearInSummary: React.FC<YearInSummaryProps> = ({ stats, selectedYear }) =>
             `📅 ${stats.activeDays} active days\n\n` +
             `🪄 Top Language: ${stats.topLanguage}\n` +
             `⚡️ Most Active Day: ${stats.mostActiveDay}\n` +
-            ` 📊 Total PRs: ${stats.totalPRs}\n` +
-            `Get your own at: https://github-freak.vercel.app/year/${selectedYear}/` +
+            `📊 Total PRs: ${stats.totalPRs}\n` +
+            `Get your own at: https://github-freak.vercel.app/year/${selectedYear}/\n` +
             `#YearInGithub #GitHub`;
         const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}`;
         window.open(url, '_blank');
